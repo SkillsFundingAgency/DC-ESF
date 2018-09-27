61680292a6a42875be5b8401aab642a08d36de19
Peter Chamley <13586812+PJChamley@users.noreply.github.com>
2018-09-27 07:45:13 +0000

    DB : Add Users DB : Add Deployment View DB : Add Pre and Post Scripts

---
+++ src/ESFA.DC.ESF.Database/Security/RoleMemberships.sql
@@ -0,0 +1,7 @@
+﻿
+GO
+ALTER ROLE [db_datawriter] ADD MEMBER [ESF_RW_User];
+GO
+ALTER ROLE [db_datareader] ADD MEMBER [ESF_RW_User];
+GO
+ALTER ROLE [db_datareader] ADD MEMBER [ESF_RO_User];
---
+++ src/ESFA.DC.ESF.Database/Security/User_RO.sql
@@ -0,0 +1,7 @@
+﻿CREATE USER [ESF_RO_User]
+    WITH PASSWORD = N'$(ROUserPassword)';
+GO
+	GRANT CONNECT TO [ESF_RO_User]
+GO
+
+
