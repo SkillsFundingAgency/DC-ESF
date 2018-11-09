a118abf210a371e2ac9b35505e0cde45f735cdf1
jamie <jamie.coales@education.gov.uk>
2018-11-09 15:06:21 +0000

    #63949 change esf funding summary report to include tabs per contract

---
+++ src/ESFA.DC.ESF.DataAccessLayer/Mappers/SourceFileModelMapper.cs
@@ -0,0 +1,22 @@
+﻿using ESFA.DC.ESF.Database.EF;
+using ESFA.DC.ESF.Interfaces.DataAccessLayer;
+using ESFA.DC.ESF.Models;
+
+namespace ESFA.DC.ESF.DataAccessLayer.Mappers
+{
+    public class SourceFileModelMapper : ISourceFileModelMapper
+    {
+        public SourceFileModel GetModelFromEntity(SourceFile entity)
+        {
+            return new SourceFileModel
+            {
+                SourceFileId = entity.SourceFileId,
+                ConRefNumber = entity.ConRefNumber,
+                UKPRN = entity.UKPRN,
+                FileName = entity.FileName,
+                PreparationDate = entity.FilePreparationDate,
+                SuppliedDate = entity.DateTime
+            };
+        }
+    }
+}
---
+++ src/ESFA.DC.ESF.Interfaces/Reports/Services/ISupplementaryDataService.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using ESFA.DC.ESF.Models;
+
+namespace ESFA.DC.ESF.Interfaces.Reports.Services
+{
+    public interface ISupplementaryDataService
+    {
+        Task<IList<SourceFileModel>> GetPreviousContractImportFilesForProvider(
+            string ukPrn,
+            CancellationToken cancellationToken);
+
+        Task<IList<SupplementaryDataModel>> GetPreviousContractDataForProvider(
+            int sourceFileId,
+            CancellationToken cancellationToken);
+    }
+}
