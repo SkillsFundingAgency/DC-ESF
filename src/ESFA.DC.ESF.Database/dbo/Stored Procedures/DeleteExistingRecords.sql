501470fc2607f92ba8af3fe87f2daa53843a9569
jamie <jamie.coales@education.gov.uk>
2018-11-22 10:53:29 +0000

    stop deleting old sourcefile records

--- src/ESFA.DC.ESF.Database/dbo/Stored Procedures/DeleteExistingRecords.sql
+++ src/ESFA.DC.ESF.Database/dbo/Stored Procedures/DeleteExistingRecords.sql
@@ -6,12 +6,12 @@ begin
 	delete sd
 	from dbo.SupplementaryData sd
 		inner join dbo.SourceFile sf on sf.SourceFileId = sd.SourceFileId
-	where sf.UKPRN = @ukprn AND sf.ConRefNumber = @conRefNum
+	where sf.UKPRN = @ukprn and sf.ConRefNumber = @conRefNum;
 	
-	DELETE ve
-	FROM dbo.ValidationError ve
-		INNER JOIN dbo.SourceFile sf ON sf.SourceFileId = ve.SourceFileId
-	WHERE sf.UKPRN = @ukprn AND sf.ConRefNumber = @conRefNum
+	delete ve
+	from dbo.ValidationError ve
+		inner join dbo.SourceFile sf on sf.SourceFileId = ve.SourceFileId
+	where sf.UKPRN = @ukprn and sf.ConRefNumber = @conRefNum;
     
-	delete from dbo.SourceFile where UKPRN = @ukprn and ConRefNumber = @conRefNum
+	--delete from dbo.SourceFile where UKPRN = @ukprn and ConRefNumber = @conRefNum;
 end
